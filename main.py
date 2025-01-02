c982cb8052ce88dcad611059ea605582b8fcb061
Samuel Binns <sambinns23@imperial.ac.uk>
2025-01-02 03:29:33 +0000

    Added cross-validation to evaluate approach

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#Main 
+#main 
 
 from typing import Literal, get_args
 import numpy as np
 import pandas as pd
 import sklearn.linear_model
 from sklearn.metrics import r2_score
+pd.options.mode.chained_assignment = None
 
 Element = Literal["fe", "c", "mn", "si", "cr", "ni", "mo", "v", "n", "nb", "co", "w", "al", "ti"]
 Property = Literal["yield_strength", "tensile_strength", "elongation"]
@@ -30,7 +31,7 @@ ELEMENT_WEIGHT: dict[str, float] = {
 }
 
 class DataframeWriter:
-    def __init__(self, data):
+    def __init__(self, data: pd.DataFrame):
         self.data = data
 
     def atom_to_weight_percent(self, formula: str):
@@ -141,6 +142,46 @@ class DataframeWriter:
         self.data['ti_calc'] = ti_calc
 
         return self.data
+    
+    @staticmethod
+    def data_shuffler(data: pd.DataFrame, n: int):
+        sorted_data = data.sort_values(["fe"], ignore_index=True)
+        length_of_segment = int(round(len(data.index) / n))
+        shuffled_fe_dict: dict[str, pd.DataFrame] = {}
+
+        for i in range(0, n-1):
+            shuffled_fe_dict["shuffled_fe_data{0}".format(i+1)] = sorted_data.iloc[i*length_of_segment:(i+1)*length_of_segment].sample(frac=1).reset_index(drop=True)
+
+        shuffled_fe_dict["shuffled_fe_data{0}".format(n)] = sorted_data.iloc[(n-1)*length_of_segment:len(data.index)].sample(frac=1).reset_index(drop=True)
+
+        fe_sample_data = {}
+        partial_sample_dict: dict[str, pd.DataFrame] = {}
+
+        for i in range(1, n):
+            sample_df = pd.DataFrame([])
+            for k in range(1, n+1):
+                partial_sample_dict["partial_sample{0}".format(k)] = shuffled_fe_dict["shuffled_fe_data{0}".format(k)].iloc[0:int(round(length_of_segment / n))]
+                sample_df = pd.concat([sample_df, partial_sample_dict["partial_sample{0}".format(k)]])
+                shuffled_fe_dict["shuffled_fe_data{0}".format(k)] = shuffled_fe_dict["shuffled_fe_data{0}".format(k)].reset_index(drop=True).drop(np.arange(0, int(round(length_of_segment / n))))
+            
+            fe_sample_data["sample{0}".format(i)] = sample_df.reset_index(drop=True)
+        
+        sample_df = pd.DataFrame([])
+
+        for k in range(1, n+1):
+            sample_df = pd.concat([sample_df, shuffled_fe_dict["shuffled_fe_data{0}".format(k)]])
+        
+        fe_sample_data["sample{0}".format(n)] = sample_df
+
+        shuffled_df = pd.DataFrame([])
+
+        for i in range(1, n+1):
+            shuffled_df = pd.concat([shuffled_df, fe_sample_data["sample{0}".format(i)]])
+        
+        shuffled_df = shuffled_df.reset_index(drop=True)
+
+        return shuffled_df
+
 
 def a_calc(data: pd.DataFrame):
     data = data.dropna()
@@ -175,6 +216,8 @@ refined_alloy_properties["elongation"] = alloy_properties["elongation"]
 refined_alloy_properties["combined_properties"] = refined_alloy_properties[[prop for prop in PROPERTIES]].values.tolist()
 refined_alloy_properties["combined_properties"] = refined_alloy_properties["combined_properties"].apply(lambda prop: np.array(prop))
 
+test_alloy_properties = refined_alloy_properties
+
 A_learned = a_calc(refined_alloy_properties)
 
 composition_vectors = refined_alloy_properties[[element for element in ELEMENT_WEIGHT.keys()]].to_numpy()
@@ -193,7 +236,7 @@ r_squared = r2_score(
     multioutput="raw_values",
 )
 
-print(r_squared)
+print("The R^2 for the entire dataset is {0}".format(r_squared))
 
 difference_array = properties_array - predicted_array
 
@@ -201,11 +244,7 @@ std_yield_strength = np.sqrt(np.sum(np.square(difference_array), axis=0)[0] / di
 std_tensile_strength = np.sqrt(np.sum(np.square(difference_array), axis=0)[1] / difference_array.shape[0])
 std_elongation = np.sqrt(np.sum(np.square(difference_array), axis=0)[2] / difference_array.shape[0])
 
-print(std_yield_strength)
-print(std_tensile_strength)
-print(std_elongation)
-
-print(refined_alloy_properties)
+print("The std for the entire dataset is {0}".format([std_yield_strength, std_tensile_strength, std_elongation]))
 
 ElongationCategory = Literal["weak", "medium", "strong", "NaN"]
 
@@ -219,10 +258,7 @@ def categorise_elongation(elongation: float) -> ElongationCategory:
     return "medium"
 
 refined_alloy_properties["elongation_catagorised_true"] = refined_alloy_properties["elongation"].apply(categorise_elongation)
-
 refined_alloy_properties["elongation_catagorised_predicted"] = refined_alloy_properties["elongation_predicted"].apply(categorise_elongation)
-
-print(refined_alloy_properties)
 
 count_have_data = 0
 count_not_equal = 0
@@ -234,4 +270,69 @@ for i in range(0, len(refined_alloy_properties.index)):
     if refined_alloy_properties.loc[i, "elongation_catagorised_true"] != refined_alloy_properties.loc[i, "elongation_catagorised_predicted"]:
         count_not_equal +=1
 
-print(count_not_equal / count_have_data)
+print("The percentage of incorrect assignments of elongation is {0}".format(count_not_equal / count_have_data))
+
+sampled_data = DataframeWriter.data_shuffler(test_alloy_properties, 3)
+
+alloy_properties_sample_1 = sampled_data.loc[0:103]
+alloy_properties_sample_2 = sampled_data.loc[104:207]
+alloy_properties_sample_3 = sampled_data.loc[208:311]
+comparison_sample = alloy_properties_sample_3.dropna().reset_index()
+
+A_sample_1 = a_calc(alloy_properties_sample_1)
+A_sample_2 = a_calc(alloy_properties_sample_2)
+
+comparison_sample["combined_predicted_1"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_1, prop))
+comparison_sample["elongation_predicted_1"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_1, prop)[2])
+comparison_sample["combined_predicted_2"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_2, prop))
+comparison_sample["elongation_predicted_2"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_2, prop)[2])
+
+properties_array = np.array(comparison_sample["combined_properties"].values.tolist())
+predicted_array_1 = np.array(comparison_sample["combined_predicted_1"].values.tolist())
+predicted_array_2 = np.array(comparison_sample["combined_predicted_2"].values.tolist())
+
+r_squared_1 = r2_score(
+    properties_array,
+    predicted_array_1,
+    multioutput="raw_values",
+)
+
+r_squared_2 = r2_score(
+    properties_array,
+    predicted_array_2,
+    multioutput="raw_values",
+)
+
+print("The R^2 for sample 1 is {0}".format(r_squared_1))
+print("The R^2 for sample 2 is {0}".format(r_squared_2))
+
+difference_array_1 = properties_array - predicted_array_1
+difference_array_2 = properties_array - predicted_array_2
+
+std_yield_strength_1 = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[0] / difference_array_1.shape[0])
+std_tensile_strength_1 = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[1] / difference_array_1.shape[0])
+std_elongation_1 = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[2] / difference_array_1.shape[0])
+std_yield_strength_2 = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[0] / difference_array_2.shape[0])
+std_tensile_strength_2 = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[1] / difference_array_2.shape[0])
+std_elongation_2 = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[2] / difference_array_2.shape[0])
+
+print("The std for sample 1 is {0}".format([std_yield_strength_1, std_tensile_strength_1, std_elongation_1]))
+print("The std for sample 2 is {0}".format([std_yield_strength_2, std_tensile_strength_2, std_elongation_2]))
+
+comparison_sample["elongation_catagorised_true"] = comparison_sample["elongation"].apply(categorise_elongation)
+comparison_sample["elongation_catagorised_predicted_1"] = comparison_sample["elongation_predicted_1"].apply(categorise_elongation)
+comparison_sample["elongation_catagorised_predicted_2"] = comparison_sample["elongation_predicted_2"].apply(categorise_elongation)
+
+count_have_data = 0
+count_not_equal_1 = 0
+count_not_equal_2 = 0
+
+for i in range(0, len(comparison_sample.index)):
+    count_have_data += 1
+    if comparison_sample.loc[i, "elongation_catagorised_true"] != comparison_sample.loc[i, "elongation_catagorised_predicted_1"]:
+        count_not_equal_1 += 1
+    elif comparison_sample.loc[i, "elongation_catagorised_true"] != comparison_sample.loc[i, "elongation_catagorised_predicted_2"]:
+        count_not_equal_2 += 1
+
+print("The percentage of incorrect assignments of elongation for sample 1 is {0}".format(count_not_equal_1 / count_have_data))
+print("The percentage of incorrect assignments of elongation for sample 2 is {0}".format(count_not_equal_2 / count_have_data))
