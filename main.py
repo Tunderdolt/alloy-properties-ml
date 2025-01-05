009719b01215babe0b1ef5035e6229be4b912940
Samuel Binns <158390101+Tunderdolt@users.noreply.github.com>
2025-01-05 14:18:58 +0000

    Changed validation script to function (#1)
    * Changed validation script to function
    
    * Made suggested changes
    
    * Improved performance of cross-validation function
    
    ---------
    
    Co-authored-by: Samuel Binns <sambinns23@imperial.ac.uk>

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ pd.options.mode.chained_assignment = None
 
 Element = Literal["fe", "c", "mn", "si", "cr", "ni", "mo", "v", "n", "nb", "co", "w", "al", "ti"]
 Property = Literal["yield_strength", "tensile_strength", "elongation"]
+ElongationCategory = Literal["weak", "medium", "strong", "NaN"]
 
 ELEMENTS: list[Element] = list(get_args(Element))
 PROPERTIES: list[Property] = list(get_args(Property))
@@ -156,33 +157,26 @@ class DataframeWriter:
 
         shuffled_fe_dict[f"shuffled_fe_data{n}"] = sorted_data.iloc[(n-1)*length_of_segment:len(data.index)].sample(frac=1).reset_index(drop=True)
 
-        fe_sample_data = {}
+        fe_sample_data: list[pd.DataFrame] = []
         partial_sample_dict: dict[str, pd.DataFrame] = {}
 
         for i in range(1, n):
             sample_df = pd.DataFrame([])
-            for k in range(1, n+1):
-                partial_sample_dict[f"partial_sample{k}"] = shuffled_fe_dict[f"shuffled_fe_data{k}"].iloc[0:int(round(length_of_segment / n))]
-                sample_df = pd.concat([sample_df, partial_sample_dict[f"partial_sample{k}"]])
-                shuffled_fe_dict[f"shuffled_fe_data{k}"] = shuffled_fe_dict[f"shuffled_fe_data{k}"].reset_index(drop=True).drop(np.arange(0, int(round(length_of_segment / n))))
+            for j in range(1, n+1):
+                partial_sample_dict[f"partial_sample{j}"] = shuffled_fe_dict[f"shuffled_fe_data{j}"].iloc[0:int(round(length_of_segment / n))]
+                sample_df = pd.concat([sample_df, partial_sample_dict[f"partial_sample{j}"]])
+                shuffled_fe_dict[f"shuffled_fe_data{j}"] = shuffled_fe_dict[f"shuffled_fe_data{j}"].reset_index(drop=True).drop(np.arange(0, int(round(length_of_segment / n))))
             
-            fe_sample_data[f"sample{i}"] = sample_df.reset_index(drop=True)
+            fe_sample_data.append(sample_df.reset_index(drop=True))
         
         sample_df = pd.DataFrame([])
 
-        for k in range(1, n+1):
-            sample_df = pd.concat([sample_df, shuffled_fe_dict[f"shuffled_fe_data{k}"]])
-        
-        fe_sample_data[f"sample{n}"] = sample_df
-
-        shuffled_df = pd.DataFrame([])
-
         for i in range(1, n+1):
-            shuffled_df = pd.concat([shuffled_df, fe_sample_data[f"sample{i}"]])
+            sample_df = pd.concat([sample_df, shuffled_fe_dict[f"shuffled_fe_data{i}"]])
         
-        shuffled_df = shuffled_df.reset_index(drop=True)
+        fe_sample_data.append(sample_df.reset_index(drop=True))
 
-        return shuffled_df
+        return fe_sample_data
 
 
 def a_calc(data: pd.DataFrame):
@@ -196,6 +190,21 @@ def a_calc(data: pd.DataFrame):
     A = model.coef_
 
     return A
+
+def calculate_mape(actual, predicted) -> float:  
+    return np.mean(np.abs(( actual - predicted) / actual), axis=0) * 100
+
+def calculate_std(actual: np.ndarray, predicted: np.ndarray, mean: float = 0):
+    return np.sqrt(np.sum(np.square(actual - predicted - mean), axis=0) / (actual - predicted).shape[0])
+
+def categorise_elongation(elongation: float) -> ElongationCategory:
+    if np.isnan(elongation):
+        return np.nan
+    if elongation < 5:
+        return "weak"
+    if elongation > 10:
+        return "strong"
+    return "medium"
         
 alloy_properties = pd.read_csv(r"C:\Users\sambi\Programming\alloy-properties-ml\database_steel_properties.csv", skiprows=1)
 alloy_properties['formula'] = alloy_properties['formula'].apply(lambda value: value.lower())
@@ -238,42 +247,15 @@ r_squared = list(r2_score(
     multioutput="raw_values",
 ))
 
-print(f"The R^2 for the entire dataset is {r_squared}")
-
 difference_array = properties_array - predicted_array
 
 std_yield_strength = np.sqrt(np.sum(np.square(difference_array), axis=0)[0] / difference_array.shape[0])
 std_tensile_strength = np.sqrt(np.sum(np.square(difference_array), axis=0)[1] / difference_array.shape[0])
 std_elongation = np.sqrt(np.sum(np.square(difference_array), axis=0)[2] / difference_array.shape[0])
 
-print(f"The std for the entire dataset is {[std_yield_strength, std_tensile_strength, std_elongation]}")
-
-def calculate_mape(actual, predicted) -> float:  
-    if not all([isinstance(actual, np.ndarray), 
-                isinstance(predicted, np.ndarray)]): 
-        actual, predicted = np.array(actual),  
-        np.array(predicted) 
-
-    return round(np.mean(np.abs(( 
-      actual - predicted) / actual)) * 100, 2)
-
 mape_ys = calculate_mape(properties_array[:,0], predicted_array[:,0])
 mape_ts = calculate_mape(properties_array[:,1], predicted_array[:,1])
 mape_e = calculate_mape(properties_array[:,2], predicted_array[:,2])
-
-print(f"The MAPE for the entire dataset is {[mape_ys, mape_ts, mape_e]}")
-
-
-ElongationCategory = Literal["weak", "medium", "strong", "NaN"]
-
-def categorise_elongation(elongation: float) -> ElongationCategory:
-    if np.isnan(elongation):
-        return np.nan
-    if elongation < 5:
-        return "weak"
-    if elongation > 10:
-        return "strong"
-    return "medium"
 
 refined_alloy_properties["elongation_catagorised_true"] = refined_alloy_properties["elongation"].apply(categorise_elongation)
 refined_alloy_properties["elongation_catagorised_predicted"] = refined_alloy_properties["elongation_predicted"].apply(categorise_elongation)
@@ -288,6 +270,9 @@ for i in range(0, len(refined_alloy_properties.index)):
     if refined_alloy_properties.loc[i, "elongation_catagorised_true"] != refined_alloy_properties.loc[i, "elongation_catagorised_predicted"]:
         count_not_equal +=1
 
+print(f"The R^2 for the entire dataset is {r_squared}")
+print(f"The std for the entire dataset is {[std_yield_strength, std_tensile_strength, std_elongation]}")
+print(f"The MAPE for the entire dataset is {[mape_ys, mape_ts, mape_e]}")
 print(f"The percentage of incorrect assignments of elongation is {count_not_equal / count_have_data}")
 
 #
@@ -486,96 +471,64 @@ plt.show()
 #The following is to validate the above model.
 #
 
-#logged_std_ys = np.zeros(10000)
-#logged_std_ts = np.zeros(10000)
-#logged_std_e = np.zeros(10000)
-#logged_r_squared_ys = np.zeros(10000)
-#logged_r_squared_ts = np.zeros(10000)
-#logged_r_squared_e = np.zeros(10000)
-#logged_mape_ys = np.zeros(10000)
-#logged_mape_ts = np.zeros(10000)
-#logged_mape_e = np.zeros(10000)
-#
-#for i in range(0, 10000, 2):
-#    sampled_data = DataframeWriter.data_shuffler(test_alloy_properties, 3)
-#    alloy_properties_sample_1 = sampled_data.loc[0:103]
-#    alloy_properties_sample_2 = sampled_data.loc[104:207]
-#    alloy_properties_sample_3 = sampled_data.loc[208:311]
-#    comparison_sample = alloy_properties_sample_3.dropna().reset_index()
-#
-#    A_sample_1 = a_calc(alloy_properties_sample_1)
-#    A_sample_2 = a_calc(alloy_properties_sample_2)
-#
-#    comparison_sample["combined_predicted_1"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_1, prop))
-#    comparison_sample["elongation_predicted_1"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_1, prop)[2])
-#    comparison_sample["combined_predicted_2"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_2, prop))
-#    comparison_sample["elongation_predicted_2"] = comparison_sample["combined_compositions"].apply(lambda prop: np.matmul(A_sample_2, prop)[2])
-#
-#    properties_array = np.array(comparison_sample["combined_properties"].values.tolist())
-#    predicted_array_1 = np.array(comparison_sample["combined_predicted_1"].values.tolist())
-#    predicted_array_2 = np.array(comparison_sample["combined_predicted_2"].values.tolist())
-#
-#    r_squared_1 = r2_score(
-#        properties_array,
-#        predicted_array_1,
-#        multioutput="raw_values",
-#    )
-#
-#    r_squared_2 = r2_score(
-#        properties_array,
-#        predicted_array_2,
-#        multioutput="raw_values",
-#    )
-#
-#    logged_r_squared_ys[i] = r_squared_1[0]
-#    logged_r_squared_ys[i+1] = r_squared_2[0]
-#    logged_r_squared_ts[i] = r_squared_1[1]
-#    logged_r_squared_ts[i+1] = r_squared_2[1]
-#    logged_r_squared_e[i] = r_squared_1[1]
-#    logged_r_squared_e[i+1] = r_squared_2[1]
-#
-#    difference_array_1 = properties_array - predicted_array_1
-#    difference_array_2 = properties_array - predicted_array_2
-#
-#    logged_std_ys[i] = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[0] / difference_array_1.shape[0])
-#    logged_std_ys[i+1] = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[0] / difference_array_1.shape[0])
-#    logged_std_ts[i] = np.sqrt(np.sum(np.square(difference_array_1), axis=0)[1] / difference_array_1.shape[0])
-#    logged_std_ts[i+1] = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[1] / difference_array_2.shape[0])
-#    logged_std_e[i] = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[2] / difference_array_2.shape[0])
-#    logged_std_e[i+1] = np.sqrt(np.sum(np.square(difference_array_2), axis=0)[2] / difference_array_2.shape[0])
-#
-#    logged_mape_ys[i] = calculate_mape(properties_array[:,0], predicted_array_1[:,0])
-#    logged_mape_ys[i+1] = calculate_mape(properties_array[:,0], predicted_array_2[:, 0])
-#    logged_mape_ts[i] = calculate_mape(properties_array[:,1], predicted_array_1[:,1])
-#    logged_mape_ts[i+1] = calculate_mape(properties_array[:,1], predicted_array_2[:, 1])
-#    logged_mape_e[i] = calculate_mape(properties_array[:,2], predicted_array_1[:,2])
-#    logged_mape_e[i+1] = calculate_mape(properties_array[:,2], predicted_array_2[:, 2])
-#
-#errors_df = pd.DataFrame([])
-#errors_df["yield_stress_R^2"] = logged_r_squared_ys
-#errors_df["tensile_stress_R^2"] = logged_r_squared_ts
-#errors_df["elongation_R^2"] = logged_r_squared_e
-#errors_df["yield_stress_std"] = logged_std_ys
-#errors_df["tensile_stress_std"] = logged_std_ts
-#errors_df["elongation_std"] = logged_std_e
-#errors_df["yield_stress_mape"] = logged_mape_ys
-#errors_df["tensile_stress_mape"] = logged_mape_ts
-#errors_df["elongation_mape"] = logged_mape_e
-#
-#figure, axis = plt.subplots(3, 3)
-#
-#sns.kdeplot(data=errors_df, x="yield_stress_R^2", clip=(0.0,1.0), ax=axis[0, 0])
-#sns.kdeplot(data=errors_df, x="tensile_stress_R^2", clip=(0.0,1.0), ax=axis[0, 1])
-#sns.kdeplot(data=errors_df, x="elongation_R^2", clip=(0.0,1.0), ax=axis[0, 2])
-#sns.kdeplot(data=errors_df, x="yield_stress_std", clip=(0.0, 500.0), ax=axis[1, 0])
-#sns.kdeplot(data=errors_df, x="tensile_stress_std", clip=(0.0, 500.0), ax=axis[1, 1])
-#sns.kdeplot(data=errors_df, x="elongation_std", clip=(0.0, 12.5), ax=axis[1, 2])
-#sns.kdeplot(data=errors_df, x="yield_stress_mape", clip=(0.0,100.0), ax=axis[2, 0])
-#sns.kdeplot(data=errors_df, x="tensile_stress_mape", clip=(0.0,100.0), ax=axis[2, 1])
-#sns.kdeplot(data=errors_df, x="elongation_mape", clip=(0.0,100.0), ax=axis[2, 2])
-#
-#plt.show()
-#
-#figure, axis = plt.subplots(7, 2)
-#
-#
+def cross_validation(n, k, data: pd.DataFrame):
+    loop_range = n * k
+    logged_std = np.zeros([3, loop_range])
+    logged_r_squared = np.zeros([3, loop_range])
+    logged_mape = np.zeros([3, loop_range])
+
+    for i in range(0, loop_range, k - 1):
+        samples: list[pd.DataFrame] = DataframeWriter.data_shuffler(data, k)
+        control_sample: pd.DataFrame = samples[0].dropna()
+        properties_array = np.array(control_sample["combined_properties"].values.tolist())
+
+        for j in range(0, k - 1):
+            A_learned = a_calc(samples[j+1])
+
+            predicted_array = np.array(
+                control_sample["combined_compositions"]
+                .apply(lambda prop: np.matmul(A_learned, prop))
+                .values
+                .tolist()
+            )
+
+            r_squared =  r2_score(
+                properties_array,
+                predicted_array,
+                multioutput="raw_values",
+            )
+
+            logged_r_squared[:, i + j] = r_squared
+
+            logged_std[:, i + j] = calculate_std(properties_array, predicted_array)
+
+            logged_mape[:, i + j] = calculate_mape(properties_array, predicted_array)
+
+    errors_df = pd.DataFrame()
+    errors_df["yield_stress_R^2"] = logged_r_squared[0]
+    errors_df["tensile_stress_R^2"] = logged_r_squared[1]
+    errors_df["elongation_R^2"] = logged_r_squared[2]
+    errors_df["yield_stress_std"] = logged_std[0]
+    errors_df["tensile_stress_std"] = logged_std[1]
+    errors_df["elongation_std"] = logged_std[2]
+    errors_df["yield_stress_mape"] = logged_mape[0]
+    errors_df["tensile_stress_mape"] = logged_mape[1]
+    errors_df["elongation_mape"] = logged_mape[2]
+
+    return errors_df
+
+errors_df = cross_validation(1000, 3, test_alloy_properties)
+
+figure, axis = plt.subplots(3, 3)
+
+sns.kdeplot(data=errors_df, x="yield_stress_R^2", clip=(0.0,1.0), ax=axis[0, 0])
+sns.kdeplot(data=errors_df, x="tensile_stress_R^2", clip=(0.0,1.0), ax=axis[0, 1])
+sns.kdeplot(data=errors_df, x="elongation_R^2", clip=(0.0,1.0), ax=axis[0, 2])
+sns.kdeplot(data=errors_df, x="yield_stress_std", clip=(0.0, 500.0), ax=axis[1, 0])
+sns.kdeplot(data=errors_df, x="tensile_stress_std", clip=(0.0, 500.0), ax=axis[1, 1])
+sns.kdeplot(data=errors_df, x="elongation_std", clip=(0.0, 12.5), ax=axis[1, 2])
+sns.kdeplot(data=errors_df, x="yield_stress_mape", clip=(0.0,100.0), ax=axis[2, 0])
+sns.kdeplot(data=errors_df, x="tensile_stress_mape", clip=(0.0,100.0), ax=axis[2, 1])
+sns.kdeplot(data=errors_df, x="elongation_mape", clip=(0.0,100.0), ax=axis[2, 2])
+
+plt.show()
